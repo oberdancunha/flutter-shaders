82ac03fecf510c48d8ea0b4ea384f6a884bf6aa3
Oberdan Cunha <oberdancunha@gmail.com>
2025-01-25 15:18:37 -0300

    Create new section called Noise.

--- lib/models/main_menu/main_menu_list.dart
+++ lib/models/main_menu/main_menu_list.dart
@@ -1,6 +1,7 @@
 import '../shaders/color_shader_list.dart';
 import '../shaders/generative_designs_shader_list.dart';
 import '../shaders/matrizes_2d_shader_list.dart';
+import '../shaders/noise_shader_list.dart';
 import '../shaders/patterns_shader_list.dart';
 import '../shaders/shape_shader_list.dart';
 import 'main_menu_model.dart';
@@ -30,5 +31,10 @@ List<MainMenuModel> get mainMenu => [
         title: 'Generative designs',
         image: 'main_menu/generative_designs.jpeg',
         shaders: GenerativeDesignsShaderList().shadersList,
+      ),
+      MainMenuModel(
+        title: 'Noise',
+        image: 'main_menu/noise.jpg',
+        shaders: NoiseShaderList().shadersList,
       ),
     ];
---
+++ lib/models/shaders/noise_shader_list.dart
@@ -0,0 +1,9 @@
+import 'shader_list.dart';
+
+final class NoiseShaderList extends ShaderList {
+  @override
+  String get mainPath => 'noise';
+
+  @override
+  int get count => 0;
+}
